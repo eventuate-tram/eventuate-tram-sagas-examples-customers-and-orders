029ada9bedcba8fc0564a96a1cb18f2899d05d09
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-04-01 12:57:08 +0300

    Supported java14

--- common-swagger/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/commonswagger/CommonSwaggerConfiguration.java
+++ common-swagger/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/commonswagger/CommonSwaggerConfiguration.java
@@ -5,16 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.WildcardType;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.concurrent.CompletableFuture;
-
-import static springfox.documentation.schema.AlternateTypeRules.newRule;
 
 @Configuration
 @EnableSwagger2
@@ -27,12 +21,7 @@ public class CommonSwaggerConfiguration {
             .apis(RequestHandlerSelectors.basePackage("io.eventuate.examples.tram.sagas.ordersandcustomers"))
             .build()
             .pathMapping("/")
-            .genericModelSubstitutes(ResponseEntity.class, CompletableFuture.class)
-            .alternateTypeRules(
-                    newRule(typeResolver.resolve(DeferredResult.class,
-                                    typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
-                            typeResolver.resolve(WildcardType.class))
-            )
+            .genericModelSubstitutes(ResponseEntity.class)
             .useDefaultResponseMessages(false)
             ;
   }
