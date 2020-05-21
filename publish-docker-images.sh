f8252d19b00e1c8033224ac375bc6eef55bf9f78
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-05-21 18:56:22 +0300

    Added development container

---
+++ publish-docker-images.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -e
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+export DOCKER_IMAGE_TAG=latest
+
+docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
+
+./gradlew assemble mysqlComposeBuild mysqlComposePush
+
+./gradlew javaDevelopmentComposePull || echo no image to pull
+
+./gradlew javaDevelopmentImageComposeBuild
+
+./gradlew javaDevelopmentImageComposePush
