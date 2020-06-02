652808bf73e4211002b5cdbf2f7d675728906ab0
Chris Richardson <chris@chrisrichardson.net>
2020-06-02 05:45:37 -0700

    Updated CircleCI to publish images

--- publish-docker-images.sh
+++ publish-docker-images.sh
@@ -4,10 +4,10 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 export DOCKER_IMAGE_TAG=latest
 
-docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
-
 ./gradlew javaDevelopmentComposePull || echo no image to pull
 
 ./gradlew javaDevelopmentImageComposeBuild
+
+docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
 ./gradlew javaDevelopmentImageComposePush
