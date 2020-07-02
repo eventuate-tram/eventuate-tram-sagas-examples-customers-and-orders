07f33a7f43dfa487298dc1fbcd72f8c2e7df4b55
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-07-02 17:55:55 +0300

    Used saga spring starters

--- customer-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/customers/CustomersServiceMain.java
+++ customer-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/customers/CustomersServiceMain.java
@@ -1,21 +1,14 @@
 package io.eventuate.examples.tram.sagas.ordersandcustomers.customers;
 
 import io.eventuate.examples.tram.sagas.ordersandcustomers.customers.web.CustomerWebConfiguration;
-import io.eventuate.tram.spring.consumer.kafka.EventuateTramKafkaMessageConsumerConfiguration;
-import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Configuration
-@Import({CustomerConfiguration.class,
-        CustomerWebConfiguration.class,
-        TramMessageProducerJdbcConfiguration.class,
-        EventuateTramKafkaMessageConsumerConfiguration.class})
-@ComponentScan
+@Import({CustomerConfiguration.class, CustomerWebConfiguration.class})
 public class CustomersServiceMain {
 
   public static void main(String[] args) {
