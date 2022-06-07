f1e009d7a8bcda01efcd97aba7eb133eff21ae74
Chris Richardson <chris@chrisrichardson.net>
2022-06-07 15:52:24 +0900

    misc fixes

--- buildSrc/src/main/groovy/ServicePlugin.groovy
+++ buildSrc/src/main/groovy/ServicePlugin.groovy
@@ -11,6 +11,7 @@ class ServicePlugin implements Plugin<Project> {
         project.dependencies {
 
             compile "org.springframework.cloud:spring-cloud-starter-sleuth"
+            compile "org.springframework.cloud:spring-cloud-sleuth-zipkin"
             compile "io.eventuate.tram.springcloudsleuth:eventuate-tram-spring-cloud-sleuth-tram-starter"
             compile "org.springdoc:springdoc-openapi-ui"
 
