fd8f28615223c161c2a3a0147bf51ce9398466dc
Chris Richardson <chris@chrisrichardson.net>
2022-06-08 14:44:56 +0900

    Added some diagnostic logging

--- .circleci/print-container-ips.sh
+++ .circleci/print-container-ips.sh
@@ -1,3 +1,3 @@
 #! /bin/bash -e
 
-docker ps -a | cut -f1 -d\ | tail +2  | xargs -n1 docker inspect -f '{{.Name}}  {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}'
+docker ps -a | cut -f1 -d\ | tail +2  | xargs -n1 docker inspect -f '{{.Name}}  {{.ID}} {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}'
