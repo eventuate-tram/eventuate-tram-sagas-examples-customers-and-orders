20da163dea6baca60988bcb87aba3e38a55e0ece
Chris Richardson <chris@chrisrichardson.net>
2022-06-08 17:11:01 +0900

    Re-attempt disabling netty DNS cache

--- api-gateway-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/apigateway/ApiGatewayMain.java
+++ api-gateway-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/apigateway/ApiGatewayMain.java
@@ -3,12 +3,18 @@ package io.eventuate.examples.tram.sagas.ordersandcustomers.apigateway;
 
 import io.eventuate.examples.tram.sagas.ordersandcustomers.apigateway.customers.CustomerDestinations;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.apigateway.orders.OrderDestinations;
+import io.netty.channel.socket.DatagramChannel;
+import io.netty.resolver.dns.DnsAddressResolverGroup;
+import io.netty.resolver.dns.DnsNameResolverBuilder;
+import io.netty.resolver.dns.NoopDnsCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.gateway.config.HttpClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.resources.LoopResources;
 import reactor.tools.agent.ReactorDebugAgent;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
@@ -19,19 +25,20 @@ public class ApiGatewayMain {
     return new ApiGatewayHealthIndicator(orderDestinations, customerDestinations, client);
   }
 
-// This seems platform specific
-//  @Bean
-//  public HttpClientCustomizer myHttpClientCustomizer() {
-//
-//    // https://github.com/spring-cloud/spring-cloud-gateway/issues/561
-//
-//    return originalHttpClient -> {
-//      DnsNameResolverBuilder dnsResolverBuilder = new DnsNameResolverBuilder()
-//              .channelFactory(NioDatagramChannel::new)
-//              .resolveCache(NoopDnsCache.INSTANCE);
-//      return originalHttpClient.resolver(new DnsAddressResolverGroup(dnsResolverBuilder));
-//    };
-//  }
+  @Bean
+  public HttpClientCustomizer myHttpClientCustomizer() {
+
+    // This seems platform specific
+    // https://github.com/spring-cloud/spring-cloud-gateway/issues/561
+
+    return originalHttpClient -> {
+      LoopResources dns = LoopResources.create("dns");
+      DnsNameResolverBuilder dnsResolverBuilder = new DnsNameResolverBuilder()
+              .channelFactory(() -> dns.onChannel(DatagramChannel.class, dns.onClient(true)))
+              .resolveCache(NoopDnsCache.INSTANCE);
+      return originalHttpClient.resolver(new DnsAddressResolverGroup(dnsResolverBuilder));
+    };
+  }
 
   public static void main(String[] args) {
     ReactorDebugAgent.init();
