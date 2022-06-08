3e2b89579a3923e21096f6f343368fc915fb6828
Chris Richardson <chris@chrisrichardson.net>
2022-06-08 15:29:37 +0900

    #59 Intermittent failure when running CustomersAndOrdersE2ETest - disable DnsCaching

--- api-gateway-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/apigateway/ApiGatewayMain.java
+++ api-gateway-service/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/apigateway/ApiGatewayMain.java
@@ -3,10 +3,15 @@ package io.eventuate.examples.tram.sagas.ordersandcustomers.apigateway;
 
 import io.eventuate.examples.tram.sagas.ordersandcustomers.apigateway.customers.CustomerDestinations;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.apigateway.orders.OrderDestinations;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.resolver.dns.DnsAddressResolverGroup;
+import io.netty.resolver.dns.DnsNameResolverBuilder;
+import io.netty.resolver.dns.NoopDnsCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.gateway.config.HttpClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.tools.agent.ReactorDebugAgent;
@@ -17,6 +22,19 @@ public class ApiGatewayMain {
   @Bean
   public ReactiveHealthIndicator apiGatewayHealthIndicator(OrderDestinations orderDestinations, CustomerDestinations customerDestinations, WebClient client) {
     return new ApiGatewayHealthIndicator(orderDestinations, customerDestinations, client);
+  }
+
+  @Bean
+  public HttpClientCustomizer myHttpClientCustomizer() {
+
+    // https://github.com/spring-cloud/spring-cloud-gateway/issues/561
+
+    return originalHttpClient -> {
+      DnsNameResolverBuilder dnsResolverBuilder = new DnsNameResolverBuilder()
+              .channelFactory(NioDatagramChannel::new)
+              .resolveCache(NoopDnsCache.INSTANCE);
+      return originalHttpClient.resolver(new DnsAddressResolverGroup(dnsResolverBuilder));
+    };
   }
 
   public static void main(String[] args) {
