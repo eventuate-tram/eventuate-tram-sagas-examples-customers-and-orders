02cec5a85265e939b6a7d2bc84d862f3322dba28
Chris Richardson <chris@chrisrichardson.net>
2022-06-24 08:43:24 -0700

    Simplify CircleCI configuration

---
+++ .circleci/generate-config.sh
@@ -0,0 +1,19 @@
+#! /bin/bash -e
+
+cat > generated_config.yml <<END
+version: 2.1
+orbs:
+  eventuate-gradle-build-and-test: "eventuate_io/eventuate-gradle-build-and-test@0.1.0"
+workflows:
+  version: 2.1
+  build-test-and-deploy:
+    jobs:
+END
+
+for build_script in build-and-test-all*.sh ; do
+cat >> generated_config.yml <<END
+      - eventuate-gradle-build-and-test/build-and-test:
+          name: $build_script
+          script: ./$build_script
+END
+done
