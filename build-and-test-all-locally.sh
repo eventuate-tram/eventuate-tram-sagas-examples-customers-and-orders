1bfbd362eb9d7cf8e7c89aaabc58aed4db2e38e2
Chris Richardson <chris@chrisrichardson.net>
2022-06-24 08:39:05 -0700

    Resolves #60 Eliminate the java development image since its not worth the additional complexity

--- build-and-test-all-locally.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash -e
-
-./java-development/build-docker-java-development-multi-arch.sh
-
-docker pull localhost:5002/eventuate-tram-sagas-examples-customers-and-orders-java-development:multi-arch-local-build
-
-./test-java-development.sh
--- test-java-development.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash -e
-
-export JAVA_DEVELOPMENT_IMAGE=localhost:5002/eventuate-tram-sagas-examples-customers-and-orders-java-development:multi-arch-local-build
-
-DC="docker-compose -f docker-compose-java-development.yml"
-$DC up -d
-$DC exec java-development docker ps
-$DC exec java-development ./gradlew tasks
-$DC exec java-development ./gradlew assemble
