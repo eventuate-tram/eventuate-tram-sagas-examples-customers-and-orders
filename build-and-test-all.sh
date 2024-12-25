a028be6e9f7a50f716b31745cc4ac9e7cb812ac3
Chris Richardson <chris@chrisrichardson.net>
2024-12-25 13:49:43 +0900

    Removed Docker Compose - replaced by  ./gradlew :end-to-end-tests:runApplication(Postgres/MySql)

--- build-and-test-all.sh
+++ build-and-test-all.sh
@@ -3,12 +3,3 @@
 set -e -o pipefail
 
 ./gradlew build
-
-./gradlew mysqlOnlyComposeUp postgresOnlyComposeUp
-
-echo 'show databases;' | ./mysql-customer-service-cli.sh -i
-echo 'show databases;' | ./mysql-order-service-cli.sh -i
-
-echo '\l' | ./postgres-cli.sh -i
-
-./gradlew mysqlOnlyComposeDown postgresOnlyComposeDown
