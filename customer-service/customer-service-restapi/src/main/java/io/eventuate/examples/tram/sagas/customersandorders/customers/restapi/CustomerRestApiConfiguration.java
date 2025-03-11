5bf5e92d7a8770f04f9c8bf579aa5207413cf7ce
Chris Richardson <chris@chrisrichardson.net>
2025-03-11 18:37:52 +0900

    Removed obsolete HttpMessageConverters code

--- customer-service/customer-service-restapi/src/main/java/io/eventuate/examples/tram/sagas/customersandorders/customers/restapi/CustomerRestApiConfiguration.java
+++ customer-service/customer-service-restapi/src/main/java/io/eventuate/examples/tram/sagas/customersandorders/customers/restapi/CustomerRestApiConfiguration.java
@@ -1,19 +1,10 @@
 package io.eventuate.examples.tram.sagas.customersandorders.customers.restapi;
 
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 @ComponentScan
 public class CustomerRestApiConfiguration {
 
-  @Bean
-  public HttpMessageConverters customConverters() {
-    HttpMessageConverter<?> additional = new MappingJackson2HttpMessageConverter();
-    return new HttpMessageConverters(additional);
-  }
 }
--- order-service/order-service-restapi/src/main/java/io/eventuate/examples/tram/sagas/customersandorders/orders/restapi/OrderRestApiConfiguration.java
+++ order-service/order-service-restapi/src/main/java/io/eventuate/examples/tram/sagas/customersandorders/orders/restapi/OrderRestApiConfiguration.java
@@ -1,20 +1,11 @@
 package io.eventuate.examples.tram.sagas.customersandorders.orders.restapi;
 
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @Configuration
 @ComponentScan
 public class OrderRestApiConfiguration {
 
-  @Bean
-  public HttpMessageConverters customConverters() {
-    HttpMessageConverter<?> additional = new MappingJackson2HttpMessageConverter();
-    return new HttpMessageConverters(additional);
-  }
 
 }
